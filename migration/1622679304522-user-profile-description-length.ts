a24eb3d506fd83aea8f860ee2faf2c22925e46a1
jaina heartles <dev@heartles.xyz>
2021-08-15 02:03:44 -0700

    Increase bio character limit
    The default character limit for user profile bios is too low, and both
    limits local users and can prevent some remote users from federating.
    This change increases the limit to 2^14 (in the ORM, for remote users)
    and 16k (for local users).
    
    Note that this currently requires manual database schema editing to take
    effect.

--- migration/1622679304522-user-profile-description-length.ts
+++ migration/1622679304522-user-profile-description-length.ts
@@ -4,7 +4,7 @@ export class userProfileDescriptionLength1622679304522 implements MigrationInter
 	name = 'userProfileDescriptionLength1622679304522';
 
 	public async up(queryRunner: QueryRunner): Promise<void> {
-		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(2048)`, undefined);
+		await queryRunner.query(`ALTER TABLE "user_profile" ALTER COLUMN "description" TYPE character varying(16384)`, undefined);
 	}
 
 	public async down(queryRunner: QueryRunner): Promise<void> {
