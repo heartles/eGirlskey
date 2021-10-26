81dccebccf870d83f28fe01d8df5862197e9a7d1
jaina heartles <dev@heartles.xyz>
2021-10-25 19:34:12 -0700

    Exclude own instance from host check

--- src/misc/is-host-blocked.ts
+++ src/misc/is-host-blocked.ts
@@ -1,6 +1,13 @@
 import { fetchMeta } from '@/misc/fetch-meta';
+import config from '@/config/index';
+import { toPuny } from '@/misc/convert-host';
+import { URL } from 'url';
 
-export async function isHostBlocked(host: string): Promise<boolean> {
+const thisHost = toPuny(new URL(config.url).hostname);
+
+export async function isHostBlocked(host: string | null): Promise<boolean> {
+	if (host == null || host === thisHost) return false;
+
 	const meta = await fetchMeta();
 
 	if (meta.allowlistMode) {
