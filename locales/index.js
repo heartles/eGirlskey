92de02489e75a6abc358c01327e604b56d22cf83
jaina heartles <dev@heartles.xyz>
2021-11-17 04:49:38 -0800

    Simplify additional pluskey strings

--- locales/index.js
+++ locales/index.js
@@ -45,20 +45,36 @@ const primaries = {
 	'zh': 'CN',
 };
 
+const pluskeyLanguages = [
+	'en-US',
+	'en-US-stock',
+];
+
 const locales = languages.reduce((a, c) => (a[c] = yaml.load(fs.readFileSync(`${__dirname}/${c}.yml`, 'utf-8')) || {}, a), {});
+const pluskeyLocales = pluskeyLanguages.reduce((a, c) => (a[c] = { pluskey: yaml.load(fs.readFileSync(`${__dirname}/pluskey/${c}.yml`, 'utf-8')) || {} }, a), {});
 
 module.exports = Object.entries(locales)
+	.map(([k, v]) => [k, merge(
+		pluskeyLocales['en-US-stock'],
+		pluskeyLocales[k],
+		v,
+	)])
 	.reduce((a, [k ,v]) => (a[k] = (() => {
 		const [lang] = k.split('-');
 		switch (k) {
 			case 'ja-JP': return v;
 			case 'ja-KS':
-			case 'en-US': return merge(locales['ja-JP'], v);
+			case 'en-US': return merge(
+				locales['ja-JP'],
+				locales['en-US-stock'],
+				v,
+			);
 			default: return merge(
 				locales['ja-JP'],
-				locales['en-US'],
+				locales['en-US-stock'],
 				locales[`${lang}-${primaries[lang]}`] || {},
 				v
 			);
 		}
 	})(), a), {});
+
